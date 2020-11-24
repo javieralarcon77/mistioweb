6f77362c410ccd14908e6c939e431f8bf26c8a4b
Javier Alarcon <javier.alarcon@pragma.com.co>
2020-11-24 14:38:16 -0500

    agregado componente de formulario  e integrado aply para multiclases con tailwind

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,23 @@
 import React from 'react';
+
 import image from '../imgs/undraw_building_websites_i78t.svg';
+import Form from './contact.form';
 
 export default () => (
     <header className="bg-gray-300">
-        <div className="container mx-auto p-12 max-w-4xl flex justify-center justify-items-center">
-            <div className="flex-1">
-                <h1 className="font-bold text-purple-700 text-6xl">Hola! Soy Javier</h1>
-                <p className="font-light text-xl">Creo aplicaciones web en diversos lenguajes de programaciones</p>
+        <div className="container mx-auto p-12 max-w-4xl">
+            <div className="flex justify-center justify-items-center">
+                <div className="flex-1">
+                    <h1 className="font-bold text-purple-700 text-6xl">Hola! Soy Javier</h1>
+                    <p className="font-light text-xl">Creo aplicaciones web en diversos lenguajes de programaciones</p>
+                </div>
+                <img
+                    style={{height:'300px'}}
+                    src={ image } alt="develop website"></img>
             </div>
-            <img
-                style={{height:'300px'}}
-                src={ image } alt="develop website"></img>
+            <div>
+                <Form />
+            </div>
         </div>
     </header>
 )
